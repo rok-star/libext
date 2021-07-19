79ca98f6a6dde1c97ae924a8a8ddaa1ae97c231f
Roman Kirsanov <kirsanov2008@gmail.com>
2021-07-19 04:52:52 +0300

    .

--- example/IO/main.cpp
+++ example/IO/main.cpp
@@ -1,24 +1,32 @@
 #include <libext/???.hpp>
 
+#define ABORT_ON_FAILURE() { \
+    if (status.type() === ext::status_type::failure) { \
+        ext::abort("%s\n", status.message().c_string()); \
+    } \
+}
+
 bool _exit = false;
 
 int main() {
 
     auto status = ext::status();
-    auto stdin = ext::io::open("//stdin", status);
-
-    if (status.type() == ext::status_type::failure) {
-        ext::abort("%s\n", status.message().c_string());
-    }
-
-    auto poller = ext::io::poller({ &stdin });
+    auto stdin = ext::io::open("//stdin", status);              ABORT_ON_FAILURE();
+    auto signal = ext::io::open("//signals", status);           ABORT_ON_FAILURE();
+    auto poller = ext::io::poller({ &stdin, &signal }, status); ABORT_ON_FAILURE();
 
     for (;;) {
         for (auto const& event : poller.poll({ .timeout = 1_ms })) {
             if (event.type() == ext::io::event_type::readable) {
-                auto chunk = event.stream.read(); // ext::array<char>
+                auto chunk = event.stream().read(); // ext::array<char> ...
 
                 ;
+            } else if (event.type() == ext::io::event_type::readable) {
+                auto signum = event.stream().signal(); // int32_t ...
+
+                if (signum == SIGINT) {
+                    break;
+                }
             }
         }
 
