6cd7cda3a2a1aa242dde19b89388980072575010
Roman Kirsanov <kirsanov2008@gmail.com>
2021-08-12 20:51:56 +0300

    .

--- example/UI/make.sh
+++ example/UI/make.sh
@@ -6,7 +6,7 @@ fi
 
 mkdir -p /tmp/libext/example/UI
 
-clang++ -std=c++2a $LIBS -lext-core -lext-ui $(dirname $0)/main.cpp -o /tmp/libext/example/UI/out.a
+clang++ -std=c++2a $(dirname $0)/main.cpp -lext-ui -lext-core $LIBS -o /tmp/libext/example/UI/out.a
 
 if [[ $@ == *"--run"* ]]; then
     /tmp/libext/example/UI/out.a
